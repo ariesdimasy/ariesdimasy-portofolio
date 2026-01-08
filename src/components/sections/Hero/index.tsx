0083320dd29a36de5c40d52e3211fac4689bbb8e
ariesdimasy <ariesdimasy>
2026-01-08 17:18:19 +0700

    update

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -1,8 +1,14 @@
 export default function Hero() {
   return (
-    <section className="min-h-screen flex flex-row">  
-        <div className="w-[50%] h-200 bg-blue-500"> </div>
-        <div className="w-[50%] h-200 bg-green-300"></div>
+    <section className="min-h-screen flex flex-row ">  
+        <div className="w-[50%] h-200 border-2 border-gray-300 p-16 bg-[#374151] rounded-2xl"> 
+          <div className="text-[#FDDE90] text-6xl font-inter-600">Aries Dimas Yudhistira</div>
+        </div>
+        <div className="w-[50%] h-200 border-2 px-8">
+          <div className="w-[50%] h-100 border-2 ">
+
+          </div>
+        </div>
     </section>
   )
 }
